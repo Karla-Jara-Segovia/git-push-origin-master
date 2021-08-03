50be47de71ef2c3e69ac767073bc5a9cbd4b9b73
karla jara karlajarasegovia@gmail.com <karlajarasegovia@gmail.com>
2021-08-03 16:32:59 -0400

    ejercicio 2

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,15 +1,4 @@
 class QuestionsController < ApplicationController
     def ask
     end
-
-    def answer
-        @question = params[:question]
-        if @question = "I am going to work"
-           @answer = "Great!"
-        elsif @question.end_with?("?")
-            @answer = "Great!"
-
-        end 
-        end
-    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  get 'answer', to: 'answer#answer'
+  get 'ask', to: 'question_#ask'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
